b96d01fa506c60f2b15846c21b2936b54c1c2b8c
ReidCampbell <reidscottcampbell@gmail.com>
2019-10-25 00:23:04 +0100

    added auth context

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { createAppContainer, createSwitchNavigator } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import { createBottomTabNavigator } from "react-navigation-tabs";
-import { StyleSheet, Text, View } from "react-native";
 
 import AccountScreen from "./src/screens/AccountScreen";
 import SigninScreen from "./src/screens/SigninScreen";
@@ -10,6 +9,7 @@ import SignupScreen from "./src/screens/SignupScreen";
 import TrackCreateScreen from "./src/screens/TrackCreateScreen";
 import TrackDetailScreen from "./src/screens/TrackDetailScreen";
 import TrackListScreen from "./src/screens/TrackListScreen";
+import { Provider as AuthProvider } from "./src/context/AuthContext";
 
 const switchNavigator = createSwitchNavigator({
     loginFlow: createStackNavigator({
@@ -26,4 +26,12 @@ const switchNavigator = createSwitchNavigator({
     })
 });
 
-export default createAppContainer(switchNavigator);
+const App = createAppContainer(switchNavigator);
+
+export default () => {
+    return (
+        <AuthProvider>
+            <App />
+        </AuthProvider>
+    );
+};
