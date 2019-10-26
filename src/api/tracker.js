15b44334b2a2b1e5da375c358a8f68d88b38657e
ReidCampbell <reidscottcampbell@gmail.com>
2019-10-26 19:42:16 +0100

    added axios api

---
+++ src/api/tracker.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export default axios.create({
+    baseURL: "https://97a999f8.ngrok.io"
+});
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { StyleSheet, View } from "react-native";
 import { Text, Input, Button } from "react-native-elements";
 
 import Spacer from "../components/Spacer";
+import { Context as AuthContext } from "../context/AuthContext";
 
 const SignupScreen = ({ navigation }) => {
+    const { state, signup } = useContext(AuthContext);
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
@@ -31,7 +33,15 @@ const SignupScreen = ({ navigation }) => {
             />
             <Spacer />
             <Spacer>
-                <Button title="Sign Up" />
+                {state.errorMessage ? (
+                    <Text style={styles.errorMessage}>
+                        {state.errorMessage}
+                    </Text>
+                ) : null}
+                <Button
+                    title="Sign Up"
+                    onPress={() => signup({ email, password })}
+                />
             </Spacer>
         </View>
     );
@@ -49,6 +59,12 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         marginBottom: 250,
         backgroundColor: "#f7797d"
+    },
+    errorMessage: {
+        fontSize: 16,
+        color: "red",
+        marginLeft: 15,
+        marginTop: 15
     }
 });
 
