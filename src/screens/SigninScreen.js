5066fa5fb756b0c0c11a4143a160fa13e529dc7c
ReidCampbell <reidscottcampbell@gmail.com>
2019-10-27 18:51:06 +0000

    added ability to clear error message

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,15 +1,17 @@
 import React, { useContext } from "react";
 import { StyleSheet, View } from "react-native";
+import { NavigationEvents } from "react-navigation";
 
 import { Context as AuthContext } from "../context/AuthContext";
 import AuthForm from "../components/AuthForm";
 import NavLink from "../components/NavLink";
 
 const SigninScreen = ({ navigation }) => {
-    const { state, signin } = useContext(AuthContext);
+    const { state, signin, clearErrorMessage } = useContext(AuthContext);
 
     return (
         <View style={styles.container}>
+            <NavigationEvents onWillBlur={clearErrorMessage} />
             <AuthForm
                 headerText="Sign in for Tracker"
                 errorMessage={state.errorMessage}
