8141b36a650dfc4635750c39622b84594eaec4ac
ReidCampbell <reidscottcampbell@gmail.com>
2019-10-30 14:54:03 +0000

    refactored hook

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { StyleSheet, ActivityIndicator } from "react-native";
-import MapView, { Polyline } from "react-native-maps";
+import MapView, { Polyline, Circle } from "react-native-maps";
 
 import { Context as LocationContext } from "../context/LocationContext";
 
@@ -21,12 +21,19 @@ const Map = () => {
                 longitudeDelta: 0.01
             }}
             style={styles.map}
-            region={{
-                ...currentLocation.coords,
-                latitudeDelta: 0.01,
-                longitudeDelta: 0.01
-            }}
-        ></MapView>
+            // region={{
+            //     ...currentLocation.coords,
+            //     latitudeDelta: 0.01,
+            //     longitudeDelta: 0.01
+            // }}
+        >
+            <Circle
+                center={currentLocation.coords}
+                radius={30}
+                strokeColor="rgba(158, 158, 255, 1.0)"
+                fillColor="rgba(158, 158, 255, 0.3)"
+            />
+        </MapView>
     );
 };
 
